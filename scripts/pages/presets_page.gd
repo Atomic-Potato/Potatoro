8c2f4e1a14a8c78fee39d220eea8834c104c78e3
Atomic Potato <medyanmehiddine@gmail.com>
2025-02-08 15:47:38 +0200

    8-2-25 PresetsPage fill presets

--- scripts/pages/presets_page.gd
+++ scripts/pages/presets_page.gd
@@ -1,10 +1,16 @@
 class_name PresetPage extends Control
 
+@export var presets_container: Container
+@export var preset_info_card_res: Resource
+
 var presets: Array[Preset]
-
-
+var cards: Array[PresetInfoCard]
 
 func _ready():
 	presets = PresetsManager.get_presets()
 	for preset in presets:
-		
+		var card: PresetInfoCard = preset_info_card_res.instantiate()
+		card.preset = preset
+		presets_container.add_child(card)
+		presets_container.move_child(card, 0)
+		cards.append(card)
