8abfd0aa9c8986d004773261b991f7e63c22fbe6
Atomic Potato <medyanmehiddine@gmail.com>
2025-02-08 15:22:12 +0200

    8-2-25 Preset info card

--- scripts/pages/components/preset_info_card.gd
+++ scripts/pages/components/preset_info_card.gd
@@ -1,2 +1,19 @@
 class_name PresetInfoCard extends Node
 
+@export var name_label: Label
+@export var tag_label: Label
+@export var sessions_lable: Label
+@export var length_lable: Label
+@export var break_lable: Label
+
+var preset: Preset:
+	set(value):
+		preset = value
+		update_text()
+		
+func update_text():
+	name_label.text = preset.name_
+	tag_label.text = str(preset.default_tag_id) # TODO: fix it, when tags are implemented
+	sessions_lable.text = str(preset.sessions_count)
+	length_lable.text = str(preset.session_length)
+	break_lable.text = str(preset.break_length)
--- scripts/pages/presets_page.gd
+++ scripts/pages/presets_page.gd
@@ -2,5 +2,9 @@ class_name PresetPage extends Control
 
 var presets: Array[Preset]
 
+
+
 func _ready():
 	presets = PresetsManager.get_presets()
+	for preset in presets:
+		
