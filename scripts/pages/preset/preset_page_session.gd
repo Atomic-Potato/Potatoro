15c2a49d5adaeac7141097181e862c592886a404
Atomic Potato <medyanmehiddine@gmail.com>
2025-02-26 01:33:05 +0200

    25-2-25 switching to break setup content

--- scripts/pages/preset/preset_page_session.gd
+++ scripts/pages/preset/preset_page_session.gd
@@ -1,27 +1,46 @@
+# NOTE: if you say to me "acshually u can make this script better this x way"
+# I know, fuk off
+
 extends Control
 
 var preset: Preset
 var session: Session
 
 @export var is_use_24_hour_format: bool = false # TODO: Get this data from the config table
-@export_category("Nodes")
+
+# NOTE: Naming abbreviation rule:
+# for nodes that are contained in content control nodes
+# i put the first letter for each word in the content node name
+# e.g: cst_lable_timer is a node in content_session_timer(_label_timer)
+@export_category("General Nodes")
 @export var label_preset_name: Label
 @export var label_sessions_count: Label
-@export var label_finish_hour: Label
-@export var label_timer: Label
+@export var content_parent: Control
 
-@export_category("Controls")
-@export var pause_toggle: CheckButton
+@export_category("Content Elements: Session Timer")
+@export var cst_label_finish_hour: Label
+@export var cst_label_timer: Label
+@export var cst_button_pause_toggle: CheckButton
+
+@export_category("Content Elements: Break Setup")
+@export var cbs_session_length_parent: Control
+@export var cbs_edit_session_length: LineEdit
+@export var cbs_edit_break_length: LineEdit
+@export var cbs_button_auto_session: CheckButton
 
 @export_category("Content")
 @export var content_default: Control
-@export var content_timer: Control
-@export var content_finish: Control
+@export var content_session_setup: Control
+@export var content_session_timer: Control
+@export var content_session_finish: Control
+@export var content_break_setup: Control
+@export var content_break_timer: Control
+@export var content_break_finish: Control
 
 var current_content: Control
 
 var update_preset: Callable = func(): preset = PresetsManager.get_preset(preset.ID)
-var set_finish_content: Callable = func(): _set_content(content_finish)
+var set_finish_content: Callable = func(): _set_content(content_session_finish)
 
 func initialize(data: Dictionary):
 	preset = data.get("preset")
@@ -33,13 +52,13 @@ func initialize(data: Dictionary):
 func _ready():
 	if SessionsManager.is_session_buffered(session.ID) \
 	and SessionsManager.get_session_id_remaining_time_in_seconds(session.ID) <= 0:
-		_set_content(content_finish)
+		_set_content(content_session_finish)
 		return
 	
 	_set_content(content_default)
 	_update_titles_text()
 	_update_timer_text()
-	if pause_toggle.button_pressed:
+	if cst_button_pause_toggle.button_pressed:
 		SessionsManager.pause_session(session.ID)
 	_update_finish_hour()
 
@@ -50,9 +69,18 @@ func _process(_delta):
 	if not SessionsManager.is_session_paused(session.ID):
 		_update_timer_text()
 
+func _set_content(content: Control):
+	if not content:
+		push_error("Cannot set null content!") 
+	if current_content:
+		current_content.visible = false
+	current_content = content
+	current_content.visible = true
+
 func load_preset_page_presets():
 	Global.AppMan.load_gui_page(Global.SceneCont.preset_page_presets)
 
+# SECTION_TITLE: Session Timer
 func add_session_length(minutes: int):
 	preset.added_session_length += minutes
 	PresetsManager.save_buffered_preset(preset, PresetsManager.get_preset_current_session_ID(preset))
@@ -75,7 +103,7 @@ func _restart_session():
 		session = SessionsManager.restart_buffered_session(session.ID)
 	else:
 		session = SessionsManager.restart_session(session.ID, preset.ID)
-		_set_content(content_timer)
+		_set_content(content_session_timer)
 	preset = PresetsManager.get_preset(preset.ID)
 	_update_finish_hour()
 	_update_titles_text()
@@ -98,14 +126,14 @@ func _update_timer_text():
 func _set_time(minutes: int, seconds: int):
 	var minutes_str = str(minutes)
 	var seconds_str = str(seconds)
-	label_timer.text = (("0" + minutes_str) if minutes < 10 else minutes_str) \
+	cst_label_timer.text = (("0" + minutes_str) if minutes < 10 else minutes_str) \
 		+ ":" \
 		+ (("0" + seconds_str) if seconds < 10 else seconds_str)
 
 func _update_finish_hour():
 	if SessionsManager.is_session_paused(session.ID) \
 	or SessionsManager.get_session_id_remaining_time_in_seconds(session.ID) <= 0:
-		label_finish_hour.text = ""
+		cst_label_finish_hour.text = ""
 		return
 	
 	var remaining_length_in_seconds = SessionsManager.get_session_id_remaining_time_in_seconds(session.ID)
@@ -117,7 +145,7 @@ func _update_finish_hour():
 	
 	if is_use_24_hour_format:
 		var finish_hour_str = str(finish_hour)
-		label_finish_hour.text = \
+		cst_label_finish_hour.text = \
 			(("0" + finish_hour_str) if finish_hour < 10 else finish_hour_str) \
 			+ ":" \
 			+ (("0" + finish_minute_str) if finish_minute < 10 else finish_minute_str)
@@ -128,16 +156,14 @@ func _update_finish_hour():
 			day_period = "pm"
 		var finish_hour_str = str(finish_hour)
 		
-		label_finish_hour.text = \
+		cst_label_finish_hour.text = \
 			(("0" + finish_hour_str) if finish_hour < 10 else finish_hour_str) \
 			+ ":" \
 			+ (("0" + finish_minute_str) if finish_minute < 10 else finish_minute_str) \
 			+ " " + day_period
 
-func _set_content(content: Control):
-	if not content:
-		push_error("Cannot set null content!") 
-	if current_content:
-		current_content.visible = false
-	current_content = content
-	current_content.visible = true
+# SECTION_TITLE: Content Break Setup
+func _set_content_break_setup():
+	_set_content(content_break_setup)
+	cbs_button_auto_session.button_pressed = preset.is_auto_start_session
+	cbs_session_length_parent.visible = not preset.is_auto_start_session
