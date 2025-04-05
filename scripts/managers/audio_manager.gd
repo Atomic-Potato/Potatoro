87bf42b152d1d1ef0f1061631bbceb8c3bc250be
Atomic Potato <medyanmehiddine@gmail.com>
2025-04-05 17:04:11 +0300

    5-4-25 Loading audio from user folder

--- scripts/managers/audio_manager.gd
+++ scripts/managers/audio_manager.gd
@@ -2,9 +2,9 @@ extends Node
 
 @export_category("Notifications")
 @export var default_session_notification: AudioStream
-var custom_session_notification_path: String
+var custom_session_notification: AudioStream
 @export var default_break_notification: AudioStream
-var custom_break_notification_path: String
+var custom_break_notification: AudioStream
 
 enum Notification
 {
@@ -16,23 +16,24 @@ var notification_player: AudioStreamPlayer
 func _ready():
 	notification_player = AudioStreamPlayer.new()
 	add_child(notification_player)
+	print(ProjectSettings.globalize_path("user://"))
 	
-	custom_session_notification_path = SettingsManager.path_session_end_notification_timer
-	custom_break_notification_path = SettingsManager.path_break_end_notification_timer
-
 func play_notification(sound: Notification):
 	stop_notification()
 	var volume: float = 0
 	match sound:
 		Notification.SessionEnd:
-			if custom_session_notification_path:
-				pass
+			if SettingsManager.path_session_end_notification_timer:
+				custom_session_notification = get_audio_stream(SettingsManager.path_session_end_notification_timer)
+				notification_player.stream = custom_session_notification
 			if not notification_player.stream:
 				notification_player.stream = default_session_notification
 			volume = SettingsManager.volume_session_end_notification
+		
 		Notification.BreakEnd:
-			if custom_break_notification_path:
-				pass
+			if SettingsManager.path_session_end_notification_timer:
+				custom_break_notification = get_audio_stream(SettingsManager.path_break_end_notification_timer)
+				notification_player.stream = custom_break_notification
 			if not notification_player.stream:
 				notification_player.stream = default_break_notification
 			volume = SettingsManager.volume_break_end_notification
@@ -50,3 +51,30 @@ func stop_notification():
 
 func is_playing_notification()-> bool:
 	return notification_player.stream != null
+
+func get_audio_stream(user_path: String)-> AudioStream:
+	user_path = "user://" + user_path
+	var file: FileAccess = FileAccess.open(user_path, FileAccess.READ)
+	if not file:
+		return null
+	var buffer: PackedByteArray = file.get_buffer(file.get_length())
+	
+	var extension: String = user_path.get_extension().to_lower()
+	var stream: AudioStream
+	match extension:
+		'mp3': 
+			stream = AudioStreamMP3.new()
+			stream.loop = true
+		'ogg': 
+			stream = AudioStreamOggVorbis.new()
+			stream.loop = true
+		'wav': 
+			stream = AudioStreamWAV.new()
+			stream.loop_mode = AudioStreamWAV.LOOP_FORWARD
+	if not stream:
+		return null
+	
+	stream.data = buffer
+	file.close()
+	
+	return stream
